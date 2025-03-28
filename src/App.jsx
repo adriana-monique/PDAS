dd626f395469346f6201cdc3105b0d37a9ca7c64
adriana-monique <perezluevanoadriana1@gmail.com>
2025-03-27 19:40:55 -0700

    agregado el componente profile card

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ProfileCard from './ProfileCard'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <section className="hero is-dark">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard 
+                title="Alexa"
+                handle="@alexa24"
+                image="https://cdn.shopify.com/s/files/1/0735/0540/7243/files/Alexa-Logo_480x480.svg?v=1713613419"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard />
+            </div>
+            <div className="column is-4">
+              <ProfileCard />
+            </div>
+          </div>
+        </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
